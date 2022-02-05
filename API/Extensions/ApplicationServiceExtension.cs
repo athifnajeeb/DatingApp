843a20354ebbc2ce35755e7764ab44fa1b85ba0f
Athif Najeeb <athifnajeeb@gmail.com>
2022-02-05 13:44:38 +0530

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.interfaces;
+using API.services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicaionServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
