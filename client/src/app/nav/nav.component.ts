6e1ddaa7aec2334f74893bd1521a38fdbe2ad1c9
Athif Najeeb <athifnajeeb@gmail.com>
2022-02-08 22:57:23 +0530

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -16,20 +16,16 @@ export class NavComponent implements OnInit {
 
   constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
-  login()
-  {
+  login() {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    })
+    }
+    )
   }
 
-  logout()
-  {
+  logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
